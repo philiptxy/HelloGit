8c67c2059855a0b844f3c22e58d63d0eb3ec83fe
Philip Teow <philipteow@Philips-MacBook-Pro.local>
2018-02-01 11:55:57 +0800

    introduce enum

--- EnumAndGit/ViewController.swift
+++ EnumAndGit/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let myGender : Gender = .both
+    let yourGender = Gender.female
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        sayHello(gender: myGender)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func sayHello(gender: Gender){
+        print(gender.rawValue)
+    }
+    
+    func func1(gender : Gender){
+        
     }
 
 
 }
 
+enum Gender : String {
+    case male
+    case female
+    case both
+    
+}
